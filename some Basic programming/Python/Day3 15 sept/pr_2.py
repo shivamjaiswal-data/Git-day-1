305e9d8a45db8e194d639faff8c51b43768a3f5d
shivamjaiswal-data <shivamjaiswal.data@gmail.com>
2026-09-15 09:57:04 +0530

    Added code of passward checker

--- some Basic programming/Python/Day3 15 sept/pr_2.py
+++ some Basic programming/Python/Day3 15 sept/pr_2.py
@@ -1,3 +1,14 @@
 while True :
     str = input("Enter a string: ")
     print(str)
+
+
+    # To check passward 
+correct_password = "some_pass"
+not_found = True
+
+while not_found:
+    passw =input("Enter the password: ")
+    if passw == correct_password:
+        not_found = False
+print("Password is correct!")
