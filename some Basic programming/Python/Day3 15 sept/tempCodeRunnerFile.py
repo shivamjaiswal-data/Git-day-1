69d46214fc33723ec532ad26f3459f5e0d75993a
shivamjaiswal-data <shivamjaiswal.data@gmail.com>
2026-09-15 10:20:22 +0530

    Added git ignore file

---
+++ some Basic programming/Python/Day3 15 sept/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+i = int(input("Enter a number to print its table: "))
+while i <= 10:
+    print(f"{i} x 2 = {i * 2}")
+    i += 1  
--- some Basic programming/Python/Day3 15 sept/while_pr_2.py
+++ some Basic programming/Python/Day3 15 sept/while_pr_2.py
@@ -9,3 +9,6 @@ i = int(input("Enter a number to print its table: "))
 while i <= 10:
     print(f"{i} x 2 = {i * 2}")
     i += 1  
+
+# Bug in Programm
+i = 0
