1d27052c7dd2098fb9b83e0cad8e94a72c0f2578
shivamjaiswal-data <shivamjaiswal.data@gmail.com>
2026-09-15 14:49:59 +0530

    Created a html

--- some Basic programming/Python/Day3 15 sept/tempCodeRunnerFile.py
+++ some Basic programming/Python/Day3 15 sept/tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
-i = 0 
-while i < 10:
-    if i == 5:
-        i += 1
-        continue
-    print(i)
-    i += 1
+lst = [1, 2, 3, 4, 5, 8, 9, 10 ] 
+i = 0
+sum = 0
+while i < len(lst):
+    sum = sum + lst[i]
+    i = i + 1 
+print("The sum of all natural number in the list is: ", sum/len(lst))
