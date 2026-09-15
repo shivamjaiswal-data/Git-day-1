c1597f378ac2ca06193da4dad66e1693df948d01
shivamjaiswal-data <shivamjaiswal.data@gmail.com>
2026-09-15 10:05:52 +0530

    Added code of table 2

---
+++ some Basic programming/Python/Day3 15 sept/while_pr_2.py
@@ -0,0 +1,11 @@
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+
+# creating a table of 2 
+i = int(input("Enter a number to print its table: "))
+while i <= 10:
+    print(f"{i} x 2 = {i * 2}")
+    i += 1  
