3978b388c0ee5649e3a34de8d9bfeb1e5fea4064
shivamjaiswal-data <shivamjaiswal.data@gmail.com>
2026-09-23 23:14:32 +0530

    I have created code of converting string upercase by alternate

---
+++ some Basic programming/Python/Day8 23 sept/alternateuppercase.py
@@ -0,0 +1,14 @@
+# we need to write a program to convert the string into alternate uppercase and lowercase letters.
+def alternate_case(s):
+    result = ""
+    for i, char in enumerate(s):
+        if i % 2 == 0:
+            result += char.upper()
+        else:
+            result += char.lower()
+    return result
+
+# Example usage
+input_string = "hello world"
+output_string = alternate_case(input_string)
+print(output_string)
